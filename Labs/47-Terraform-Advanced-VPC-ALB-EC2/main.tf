949611eba2cd399639172fa47923ff6abf6b9b69
Leszek Ucinski <rahvinos@gmail.com>
2021-11-14 11:07:23 +0100

    day 47

---
+++ Labs/47-Terraform-Advanced-VPC-ALB-EC2/main.tf
@@ -0,0 +1,305 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.65.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical
+  owners = ["099720109477"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "CloudAcademy"
+    Demo = "Terraform"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone = var.availability_zones[0]
+
+  tags = {
+    Name = "Subnet1"
+    Type = "Public"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "Subnet2"
+    Type = "Public"
+  }
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = var.availability_zones[0]
+
+  tags = {
+    Name = "Subnet3"
+    Type = "Private"
+  }
+}
+
+resource "aws_subnet" "subnet4" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "Subnet4"
+    Type = "Private"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name"  = "Main"
+    "Owner" = "CloudAcademy"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnet1.id
+
+  tags = {
+    Name = "NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_route_table" "rt2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private"
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_route_table_association" "rta3" {
+  subnet_id      = aws_subnet.subnet3.id
+  route_table_id = aws_route_table.rt2.id
+}
+
+resource "aws_route_table_association" "rta4" {
+  subnet_id      = aws_subnet.subnet4.id
+  route_table_id = aws_route_table.rt2.id
+}
+
+resource "aws_security_group" "webserver" {
+  name        = "webserver"
+  description = "webserver network traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.workstation_ip]
+  }
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      cidrsubnet(var.cidr_block, 8, 1),
+      cidrsubnet(var.cidr_block, 8, 2)
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow traffic"
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb"
+  description = "alb network traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.webserver.id]
+  }
+
+  tags = {
+    Name = "allow traffic"
+  }
+}
+
+resource "aws_launch_template" "launchtemplate1" {
+  name = "web"
+
+  image_id               = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "WebServer"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ec2.userdata")
+}
+
+resource "aws_lb" "alb1" {
+  name               = "alb1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  enable_deletion_protection = false
+
+  /*
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+  */
+
+  tags = {
+    Environment = "Prod"
+  }
+}
+
+resource "aws_alb_target_group" "webserver" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb1.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.webserver.arn
+  }
+}
+
+resource "aws_alb_listener_rule" "rule1" {
+  listener_arn = aws_alb_listener.front_end.arn
+  priority     = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.webserver.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  vpc_zone_identifier = [aws_subnet.subnet3.id, aws_subnet.subnet4.id]
+
+  desired_capacity = 2
+  max_size         = 2
+  min_size         = 2
+
+  target_group_arns = [aws_alb_target_group.webserver.arn]
+
+  launch_template {
+    id      = aws_launch_template.launchtemplate1.id
+    version = "$Latest"
+  }
+}
